893be335eddf60535ff8f5da4ad1f4e0ef18e64f
github username <bobjin42@gmail.com>
2018-10-09 23:29:21 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,28 @@
 // Code DigitalClicker Component Here
+import React, { Component, Fragment } from 'react';
+
+class DigitalClicker extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      timesClicked: 0,
+    }
+  }
+
+  handlerClick = () => {
+    const update = this.state.timesClicked + 1
+    this.setState({
+      timesClicked: update
+    })
+  }
+
+  render() {
+    return (
+      <button onClick={this.handlerClick}>{this.state.timesClicked}</button>
+    );
+  }
+
+}
+
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,44 @@
 // Code YouTubeDebugger Component Here
+import React, { Component, Fragment } from 'react';
+
+class YouTubeDebugger extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      errors: [],
+      user: null,
+      settings: { bitrate: 8, video: { resolution: '1080p' } }
+    }
+  }
+
+  handlerClick = () => {
+    this.setState({
+      settings: {...this.state.settings,
+        bitrate: 12,
+      }
+    })
+  }
+
+  change = () => {
+    this.setState({
+      settings: {...this.state.settings,
+        video: {...this.state.settings.video,
+          resolution: '720p'
+        }
+      }
+    })
+  }
+
+  render() {
+    return (
+      <Fragment>
+      <button className = "bitrate" onClick={this.handlerClick}></button>
+      <button className = "resolution" onClick={this.change}></button>
+      </Fragment>
+    );
+  }
+
+}
+
+export default YouTubeDebugger;
